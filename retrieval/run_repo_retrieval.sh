2245f9c92b94ed22bf34671fbcc0d77a17772fe8
Jason Zhang <71229867+yilinjz@users.noreply.github.com>
2024-10-13 22:06:01 -0400

    refactor

--- retrieval/run_repo_retrieval.sh
+++ retrieval/run_repo_retrieval.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 python eval_beir_pyserini_repo.py \
   --dataset repoeval \
-  --output_metadir repo_retrieval/repoeval/post_ast/project-x-corpora/ \
-  --index_path repo_retrieval/repoeval/post_ast/bm25_indices/ \
+  --output_metadir bm25_retrieval_data/repoeval/post_ast/project-x-corpora/ \
+  --index_path bm25_retrieval_data/repoeval/post_ast/bm25_indices/ \
   --top_k 10 \
   --k1 1.2 \
   --b 0.75 \
-  --output_file repo_retrieval/repoeval/post_ast/eval.json \
-  --results_file repo_retrieval/repoeval/post_ast/results.json
+  --output_file bm25_retrieval_data/repoeval/post_ast/eval.json \
+  --results_file bm25_retrieval_data/repoeval/post_ast/results.json
